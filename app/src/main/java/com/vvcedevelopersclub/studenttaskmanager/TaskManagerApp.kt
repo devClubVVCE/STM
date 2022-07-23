7307e1c1772654bc3f1f2b9345fa545856c615c3
Ruthvik B.R <ruthvikbr24@gmail.com>
2022-07-23 12:01:44 +0530

    Revert "Feat/dashboard screen"

--- app/src/main/java/com/vvcedevelopersclub/studenttaskmanager/TaskManagerApp.kt
+++
@@ -1,8 +0,0 @@
-package com.vvcedevelopersclub.studenttaskmanager
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class TaskManagerApp : Application() {
-}
--- app/src/main/java/com/vvcedevelopersclub/studenttaskmanager/ui/utils/Screens.kt
+++
@@ -1,6 +0,0 @@
-package com.vvcedevelopersclub.studenttaskmanager.ui.utils
-
-sealed class Screen(val route:String){
-    object TasksScreen:Screen("Tasks")
-    object AddEditTasksScreen:Screen("AddEditTasks")
-}
